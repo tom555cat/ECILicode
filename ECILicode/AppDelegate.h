206dbede81771bd8ebce033c5c706e51d8fdedaf
tom555cat <tongleiming1989@mail.com>
2017-07-27 13:43:20 +0800

    Initial Commit

---
+++ ECILicode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ECILicode
+//
+//  Created by MAC-MiNi on 2017/7/27.
+//  Copyright © 2017年 MAC-MiNi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ECILicode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ECILicode
+//
+//  Created by MAC-MiNi on 2017/7/27.
+//  Copyright © 2017年 MAC-MiNi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
